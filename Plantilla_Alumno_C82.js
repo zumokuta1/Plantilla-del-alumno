04529803adff86a321bacef02788f75810c1be46
zumokuta1 <131922106+zumokuta1@users.noreply.github.com>
2023-10-04 18:04:23 -0500

    s

---
+++ Plantilla_Alumno_C82.js
@@ -0,0 +1,52 @@
+console.log("My first console project");
+
+var first_name = "Ranbir";
+var last_name = "Kapoor";
+/*
+Concatenar el primer nombre y apellido para imprimir el nombre completo de la persona en la consola. 
+
+Escribir la línea: "first_name.concat(last_name)"
+*/
+var full_name = first_name.concat;
+console.log(full_name);
+
+var a = 10;
+var b = 5;
+/*
+Crear una variable llamada add y guardar el valor de la suma de a y b en esta variable.
+
+Escribir: var add = a + b
+
+Después, imprime la variable para mostrar el valor de la suma.
+
+Escribir: console.log(add)
+*/
+var add = a + b;
+console.log(add);
+
+/*
+Crear una variable llamada sub y guardar aquí la el valor de la resta de a y b.
+
+Escribir: var sub = a - b
+
+Después, imprimie el valor para mostrar el valor de la resta.
+
+Escribir: console.log(sub)
+*/
+var sub = a - b;
+console.log(sub);
+
+/*
+Crear una variable llamada multiple y guardar aquí el valor de la multiplicación de a y b.
+
+Escribir: var multiple = a * b
+
+Después, imprime la variable para mostrar el valor de la multiplicación.
+
+Escribir: console.log(multiple)
+*/
+var multiple = a * b;
+console.log(multiple);
+
+var divide = a / b;
+console.log(divide);
